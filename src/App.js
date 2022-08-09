8fef219fe0e823fae1751e45f6af7a1ff8e99c60
abhirup bardhan <bardhan.abhirup@gmail.com>
2022-08-09 15:44:00 +0530

    complete 1 block at a time implementation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import data from './data';
 import './App.css';
 import { useState } from 'react';
-import Accordion from './Accordion';
 
 function App() {
-  // const [expand, setExpand] = useState(false);
+  const [itemToDisplay, setItemToDisplay] = useState(null);
+
+  const handleClick = (id) => {
+    if (id === itemToDisplay) {
+      return setItemToDisplay(null);
+    }
+    setItemToDisplay(id);
+  };
 
   return (
     <div className='main-container'>
       <h1>My Content</h1>
       <div className='qna-container'>
         {data.map(({ id, title, content }) => (
-          <Accordion key={id} title={title} content={content} />
+          <div className='block' key={id}>
+            <div className='title' onClick={() => handleClick(id)}>
+              {title}
+              <span className='expand'>{id !== itemToDisplay ? '+' : '-'}</span>
+            </div>
+            <div className={`content ${id !== itemToDisplay && 'hide'}`}>
+              {content}
+            </div>
+          </div>
         ))}
       </div>
     </div>
