dc37ee3e94ad83fe6eeb115e5a7113ab6f3ae581
abhirup bardhan <bardhan.abhirup@gmail.com>
2022-08-09 15:31:55 +0530

    complete accordion

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import data from './data';
+import './App.css';
+import { useState } from 'react';
+import Accordion from './Accordion';
+
 function App() {
-  return <div>Hello world</div>;
+  // const [expand, setExpand] = useState(false);
+
+  return (
+    <div className='main-container'>
+      <h1>My Content</h1>
+      <div className='qna-container'>
+        {data.map(({ id, title, content }) => (
+          <Accordion key={id} title={title} content={content} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,46 @@
+const data = [
+  {
+    id: 1,
+    title: 'Lorem ipsum dolor sit amet?',
+    content:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus provident aut eveniet rem enim, officia facere vitae rerum consequuntur nemo repudiandae! Ab error molestiae consequatur molestias nemo. Fugit tempore adipisci possimus repellendus harum voluptates quam rem nostrum doloribus architecto sapiente voluptatum tenetur animi saepe qui odio quisquam optio, sed culpa.',
+  },
+  {
+    id: 2,
+    title: 'Lorem ipsum dolor sit amet?',
+    content:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus provident aut eveniet rem enim, officia facere vitae rerum consequuntur nemo repudiandae! Ab error molestiae consequatur molestias nemo. Fugit tempore adipisci possimus repellendus harum voluptates quam rem nostrum doloribus architecto sapiente voluptatum tenetur animi saepe qui odio quisquam optio, sed culpa.',
+  },
+  {
+    id: 3,
+    title: 'Lorem ipsum dolor sit amet?',
+    content:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus provident aut eveniet rem enim, officia facere vitae rerum consequuntur nemo repudiandae! Ab error molestiae consequatur molestias nemo. Fugit tempore adipisci possimus repellendus harum voluptates quam rem nostrum doloribus architecto sapiente voluptatum tenetur animi saepe qui odio quisquam optio, sed culpa.',
+  },
+  {
+    id: 4,
+    title: 'Lorem ipsum dolor sit amet?',
+    content:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus provident aut eveniet rem enim, officia facere vitae rerum consequuntur nemo repudiandae! Ab error molestiae consequatur molestias nemo. Fugit tempore adipisci possimus repellendus harum voluptates quam rem nostrum doloribus architecto sapiente voluptatum tenetur animi saepe qui odio quisquam optio, sed culpa.',
+  },
+  {
+    id: 5,
+    title: 'Lorem ipsum dolor sit amet?',
+    content:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus provident aut eveniet rem enim, officia facere vitae rerum consequuntur nemo repudiandae! Ab error molestiae consequatur molestias nemo. Fugit tempore adipisci possimus repellendus harum voluptates quam rem nostrum doloribus architecto sapiente voluptatum tenetur animi saepe qui odio quisquam optio, sed culpa.',
+  },
+  {
+    id: 6,
+    title: 'Lorem ipsum dolor sit amet?',
+    content:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus provident aut eveniet rem enim, officia facere vitae rerum consequuntur nemo repudiandae! Ab error molestiae consequatur molestias nemo. Fugit tempore adipisci possimus repellendus harum voluptates quam rem nostrum doloribus architecto sapiente voluptatum tenetur animi saepe qui odio quisquam optio, sed culpa.',
+  },
+  {
+    id: 7,
+    title: 'Lorem ipsum dolor sit amet?',
+    content:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus provident aut eveniet rem enim, officia facere vitae rerum consequuntur nemo repudiandae! Ab error molestiae consequatur molestias nemo. Fugit tempore adipisci possimus repellendus harum voluptates quam rem nostrum doloribus architecto sapiente voluptatum tenetur animi saepe qui odio quisquam optio, sed culpa.',
+  },
+];
+
+export default data;
