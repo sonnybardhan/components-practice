05a1adc6d3dd41271624335e8bf66a6694b20aee
abhirup bardhan <bardhan.abhirup@gmail.com>
2022-08-14 23:04:34 +0530

    complete basic drag and drop implementation

--- src/DragAndDrop.js
+++ src/DragAndDrop.js
@@ -1,14 +1,51 @@
 import React, { useRef, useState } from 'react';
-import Block from './Block';
 
 const DragAndDrop = ({ dataList }) => {
   const [data, setData] = useState(dataList);
+  const dragItemIdx = useRef();
+  const dragOverItemIdx = useRef();
+
+  function handleDragStart(e, idx) {
+    dragItemIdx.current = idx;
+  }
+
+  function handleDragEnter(e, idx) {
+    if (idx !== dragItemIdx.current) {
+      dragOverItemIdx.current = idx;
+    }
+  }
+
+  function swap() {
+    const _list = [...data];
+    const _dragItem = _list.splice(dragItemIdx.current, 1)[0];
+    _list.splice(dragOverItemIdx.current, 0, _dragItem);
+
+    dragItemIdx.current = null;
+    dragOverItemIdx.current = null;
+    setData(_list);
+  }
+
+  function handleDragEnd(e) {
+    swap();
+  }
 
   return (
     <div className='dnd-group'>
       {data &&
-        data.map((item) => {
-          return <Block key={Math.random()} item={item} />;
+        data.map((item, idx) => {
+          return (
+            <div
+              className='block'
+              draggable
+              onDragStart={(e) => handleDragStart(e, idx)}
+              onDragEnter={(e) => handleDragEnter(e, idx)}
+              onDragOver={(e) => e.preventDefault()}
+              onDragEnd={(e) => handleDragEnd(e)}
+              key={Math.random()}
+            >
+              {item.name}
+            </div>
+          );
         })}
     </div>
   );
