ebe6622ca61c299a8212505e1405c1ce9225dfb7
abhirup bardhan <bardhan.abhirup@gmail.com>
2022-08-15 12:44:02 +0530

    implement basic modal

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import { useState } from 'react';
+import { createPortal } from 'react-dom';
+import './App.css';
+
 function App() {
-  return <div>Hello world</div>;
+  const [show, setShow] = useState(false);
+
+  return (
+    <div className='App'>
+      <h1>Modal Demo</h1>
+      <div className='container' onClick={() => console.log('clicked')}>
+        <button onClick={() => setShow(!show)}>{show ? 'Hide' : 'Show'}</button>
+        {show && <Modal show={show} setShow={setShow} />}
+      </div>
+      <p className='other'>Other content</p>
+    </div>
+  );
+}
+
+function Modal({ show, setShow }) {
+  return createPortal(
+    <p className='modal'>
+      <button onClick={() => setShow(!show)}>{show ? 'Hide' : 'Show'}</button>
+    </p>,
+    document.getElementById('modal-root')
+  );
 }
 
 export default App;
