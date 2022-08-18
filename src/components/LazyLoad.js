c30fb57c82287d64fce33e1c04e8900afe043e31
abhirup bardhan <bardhan.abhirup@gmail.com>
2022-08-19 03:48:22 +0530

    improve lazy-load

--- src/components/LazyLoad.js
+++ src/components/LazyLoad.js
@@ -1,64 +1,50 @@
 import './LazyLoad.css';
-import { useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import ContentBlock from './ContentBlock';
 // const URL = `https://jsonplaceholder.typicode.com/posts?_limit=10&_start=5`;
 const URL = `https://jsonplaceholder.typicode.com/posts?_limit=10`;
 
-// const allPosts = [];
-
 const LazyLoad = () => {
-  const allPosts = useRef();
-  const [posts, setPosts] = useState(allPosts);
+  const [posts, setPosts] = useState([]);
   const [lastId, setLastId] = useState(0);
+  const [getMorePosts, setGetMorePosts] = useState(false);
+
+  const fetchPosts = useCallback(async () => {
+    const res = await fetch(URL + '&_start=' + lastId);
+    const fetchedPosts = await res.json();
+    setPosts((previousPosts) => [...previousPosts, ...fetchedPosts]);
+    setLastId(fetchedPosts[fetchedPosts.length - 1].id);
+    setGetMorePosts(false);
+  }, [lastId]);
 
   useEffect(() => {
-    const fetchPosts = async () => {
-      const res = await fetch(URL);
-      const fetchedPosts = await res.json();
-      setPosts(fetchedPosts);
-      allPosts.current = fetchedPosts;
-    };
+    fetchPosts();
+  }, []);
 
-    if (allPosts.current) {
-      setPosts(allPosts.current);
-    } else {
+  useEffect(() => {
+    if (posts.length) {
+      setLastId(posts[posts.length - 1].id);
+    }
+  }, [posts]);
+
+  useEffect(() => {
+    if (getMorePosts) {
       fetchPosts();
     }
-  }, []);
+  }, [getMorePosts]);
 
   useEffect(() => {
     const handleScroll = (e) => {
       const pageHeight = document.documentElement.clientHeight;
       const scrollTop = document.documentElement.scrollTop;
       const scrollHeight = document.documentElement.scrollHeight;
-
       if (pageHeight + scrollTop >= scrollHeight - 20) {
-        loadPosts();
+        setGetMorePosts(true);
       }
     };
-
     window.addEventListener('scroll', handleScroll);
-
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
-
-  useEffect(() => {
-    if (allPosts.current?.length) {
-      console.log('allPosts.length: ', allPosts.current.length);
-      const lastPostId = allPosts.current[allPosts.current.length - 1].id;
-      console.log('id updated: ', lastPostId);
-      setLastId(lastPostId);
-      console.log('lastPostId: ', lastPostId);
-    }
-  }, []);
-
-  async function loadPosts() {
-    const lastPostId = allPosts.current[allPosts.current.length - 1].id;
-    const res = await fetch(`${URL}&_start=${lastPostId}`);
-    const newPosts = await res.json();
-    allPosts.current.push(...newPosts);
-    setPosts([...allPosts.current]);
-  }
 
   return (
     <div className='lazy-load-container'>
